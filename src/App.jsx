86aa3b77ec8ff3bf77e6389f58017b8727461dec
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-05-08 14:22:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+  const [country , setCountry] = useState([])
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountry(data))
+    .catch(e => console.error(e))
+  } , [])
+
+  console.log(country);
+
+  return (
+    <section className='wrapper' >
+      {country.length !==0 ? 
+        country.map(ele => (
+          <div 
+            key={ele.name.common}
+            className='div'
+          >
+              <img className='img' src={ele.flags.png} onerror={`this.onerror=null;this.src=${ele.flags.svg};`} alt="" />
+              <p>{ele.name.common}</p>
+          </div>
+        ))
+      : 
+      (
+        <h1 >Loading...</h1>
+      )}
+    </section>
+  )
+}
+
+export default App
