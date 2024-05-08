9d40a2e9fee8583810ab54389d39fa393cf36943
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-05-08 14:35:54 +0530

    update the file

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
             key={ele.name.common}
             className='div'
           >
-              <img className='img' src={ele.flags.png} onerror={`this.onerror=null;this.src=${ele.flags.svg};`} alt="" />
+              <img className='img' src={ele.flags.png}  alt="" />
               <p>{ele.name.common}</p>
           </div>
         ))
